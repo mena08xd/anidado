de61801c8a9c3cafc8e3bc477e75a4a74ac45c99
Ximena Valenzuela <xduarte@uabc.edu.mx>
2022-05-27 01:48:33 -0700

    Agregar archivos de proyecto.

---
+++ anidado/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anidado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            int resultado;
+
+            for (i = 1; i <= 10; i++)
+
+            {
+                Console.WriteLine("Tabla de multiplicar del {0}", i);
+                for (j = 1; j <= 10; j++)
+                {
+                    resultado = i * j;
+                    Console.WriteLine("{0} * {1} = {2}", i, j, resultado);
+                    Console.ReadKey();
+
+
+
+
+
+
+
+
+
+                }
+            }
+        }
+    }
+}
